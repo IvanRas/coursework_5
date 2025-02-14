11e369d9159b4ffd82b8bc9da134192f9cd6ccac
root <rastorgu98@gmail.com>
2025-02-14 12:18:01 +0300

    update project 1.2.3

--- habits/models.py
+++ habits/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from config import settings
 
@@ -7,26 +8,6 @@ from config import settings
 
 
 class Habits(models.Model):
-    place = (models.CharField(max_length=250, verbose_name="Место выполнения", help_text="Введите Место"),)
-    time_to_perform = (models.TimeField(auto_now=True, verbose_name="Время на выполнение", blank=True, null=True),)
-    pleasant_habit_flag = (
-        models.BooleanField(max_length=250, verbose_name="Признак приятной привычки", help_text="Введите Признак"),
-    )
-
-    linked_habit = (
-        models.BooleanField(
-            max_length=250,
-            null=True,
-            blank=True,
-            verbose_name="Связанная привычка",
-        ),
-    )
-    frequency = models.PositiveIntegerField(default=1, verbose_name="Периодичность")
-    reward = models.CharField(
-        max_length=250, null=True, blank=True, verbose_name="Вознаграждение ", help_text="Введите Вознаграждение "
-    )
-    publicity_flag = (models.BooleanField(default=False, verbose_name="Признак публичности"),)
-    is_available = (models.BooleanField(default=False, verbose_name="Работает"),)
     owner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         null=True,
@@ -35,12 +16,37 @@ class Habits(models.Model):
         related_name="habits",
         verbose_name="Создатель привычки",
     )
+    place = models.CharField(max_length=250, verbose_name="Место выполнения", help_text="Введите Место")
+    time = models.TimeField(verbose_name="Время выполнения")
+    action = models.CharField(max_length=255, verbose_name="Действие")
+    pleasant_habit_flag = models.BooleanField(
+        max_length=250, verbose_name="Признак приятной привычки", help_text="Введите Признак"
+    )
+    related_habit = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        limit_choices_to={"pleasant_habit_flag": True},
+        verbose_name="Связанная привычка",
+    )  # ForeignKey self
+    frequency = models.PositiveIntegerField(default=1, verbose_name="Периодичность")
+    reward = models.CharField(
+        max_length=250, null=True, blank=True, verbose_name="Вознаграждение ", help_text="Введите Вознаграждение "
+    )
+    time_to_perform = models.PositiveIntegerField(verbose_name="Время на выполнение", blank=True, null=True)  # Int+
+    publicity_flag = models.BooleanField(default=False, verbose_name="Признак публичности")
 
     def __str__(self):
         return f"{self.pleasant_habit_flag}, {self.linked_habit}"
 
     def clean(self):
-        pass
+        if self.reward and self.related_habit:
+            raise ValidationError("Нельзя совмещать вознаграждение и связанную привычку.")
+        if self.time_to_perform > 120:
+            raise ValidationError("Время выполнения не должно быть больше 120 минут.")
+        if self.frequency > 7:
+            raise ValidationError("Периодичность выполнения должна быть от 1 до 7 дней")
 
     class Meta:
         verbose_name = "Привычка"
--- habits/views.py
+++ habits/views.py
@@ -29,7 +29,7 @@ class PublicHabitListView(ListAPIView):
     serializer_class = HabitsSerializer
 
     def get_queryset(self):
-        return Habits.objects.filter(action=True).order_by("action")
+        return Habits.objects.filter(action=True)
 
 
 class HabitCreateView(CreateAPIView):
