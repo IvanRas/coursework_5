44c86542cd5777a86ff3d9f12d16e4873c54ac49
root <rastorgu98@gmail.com>
2025-02-15 12:07:44 +0300

    update project 1.3.3 (test)

--- habits/tests.py
+++ habits/tests.py
@@ -1,19 +1,46 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-from .models import Habits
-from users.models import User  # импорт новой модели User
+
+from habits.models import Habits
 
 # Create your tests here.
 
+User = get_user_model()
 
-class HabitsAPITestCase(APITestCase):
+
+class HabitsModelTest(TestCase):
     def setUp(self):
-        # Создаем тестового пользователя с использованием email
-        self.user = User.objects.create_user(
-            email="testuser@example.com",
-            username="testusername",
-            password="password",
-            is_staff=True,
+        self. user = User.objects.create_user(username="test_user", password="12345")
+
+    def test_creating_habit(self):
+        habits = Habits.objects.create(
+            place="место",
+            time="14:00:00",
+            action="Действие",
+            pleasant_habit_flag=True,
+            frequency=2,
+            reward="Вознаграждение",
+            time_to_perform=100,
+            publicity_flag=True,
+            owner= self.user,
+            related_habit="Связанная привычка"
         )
+        self.assertIsInstance(habits, Habits)
+        self.assertEqual(habits.action, "Действие")
+        self.assertEqual(habits.place, "место")
+        self.assertEqual(habits.frequency, 2)
+
+    def test_habit_string_representation(self):
+        habits = Habits.objects.create(
+            place="место",
+            time="14:00:00",
+            action="Действие",
+            pleasant_habit_flag=True,
+            frequency=2,
+            reward="Вознаграждение",
+            time_to_perform=100,
+            publicity_flag=True,
+            owner= self.user,
+            related_habit="Связанная привычка"
+        )
+        self.assertEqual(str(habits), habits.action)
