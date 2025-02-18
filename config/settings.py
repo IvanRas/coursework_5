7f8e89bc6b5425a07350e6f1bf28db3142482288
root <rastorgu98@gmail.com>
2025-02-18 18:39:10 +0300

    update project 1.4.3 (final 0.5)

--- config/settings.py
+++ config/settings.py
@@ -186,7 +186,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://read-and-write.example.com",  # Замените на адрес вашего фронтенд-сервера и добавьте адрес бэкенд-сервера
+    "http://localhost:8000",  # Замените на адрес вашего фронтенд-сервера и добавьте адрес бэкенд-сервера
 ]
 
 CORS_ALLOW_ALL_ORIGINS = False
